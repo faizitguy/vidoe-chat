2d6579576c39bfe3f6dfcd0816e129c651507065
faizitguy <faizkhan3597@gmail.com>
2021-06-27 18:37:25 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and listening on port ${PORT}`);
+});
